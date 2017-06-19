fa0ef3e745a4536bb80285125156ec27965ed3ce
samuel <samh386@gmail.com>
2017-06-19 10:18:17 -0600

    added follow2

---
+++ follow2.py
@@ -0,0 +1,36 @@
+from robot import get_robot_position, move_to_point
+from commands import get_speed, where_robot, where_markers, where_all
+from prediction import get_estimated_position
+from math import atan2
+from time import sleep
+import commands
+
+ADDRESS = ("0.0.0.0", 55555)
+
+commands.open_connection(ADDRESS)
+
+markers = where_markers()
+waypoints = []
+
+for marker_number in markers:
+    if (marker_number == "time" or marker_number == "robot"):
+        continue
+    waypoints.append(markers[marker_number]["center"])
+
+states = []
+timestamps = []
+
+while len(waypoints) > 0:
+    robot_speed = get_speed()
+    states.append([robot_speed['speed_a'], robot_speed['speed_b']])
+    timestamps.append(float(where_all()['time']))
+    if len(states) > 3:
+        states.pop(0)
+        timestamps.pop(0)
+    sensor_position = get_robot_position()
+    sensor_orientation = where_robot()['orientation']
+    angle = atan2(sensor_orientation[1], sensor_orientation[0])
+    (x, y, new_angle) = get_estimated_position(states, [sensor_position[0], sensor_position[1], angle], timestamps)
+    if move_to_point((waypoints[0][0], waypoints[0][1]), (x, y), new_angle, 8):
+        waypoints.pop(0)
+    sleep(.3) #the follow_vector also sleeps for 0.05 seconds
---
+++ prediction2.py
@@ -0,0 +1,33 @@
+from kalman2 import *
+import numpy
+import math
+
+MAX_TIME = 0.8
+
+def get_estimated_position(states, start, timestamps):
+    total_time = 0.0
+    X = array([[start[0]], [start[1]], [states[0]], [states[1]]])
+    P = diag((0.01, 0.01, 0.01, 0.01))
+    for i in range(0, len(states) - 1):
+        delta_t = timestamps[i + 1] - timestamps[i]
+        total_time += delta_t
+        if (total_time > MAX_TIME):
+            return [X[0], X[1]]
+        (X, P) = get_next_state(X, P, states[i], delta_t)
+    return [X[0], X[1]]
+
+def get_next_state(X, P, state, delta_t):
+    A = array([[1, 0, delta_t , 0], [0, 1, 0, delta_t], [0, 0, 1, 0], [0, 0, 0, 1]])
+    Q = eye(X.shape[0])
+    B = eye(X.shape[0])
+    U = zeros((X.shape[0],1)) 
+    
+    Y = array([[state[0]], [state[1]]])
+    H = array([[0, 0, 1, 0], [0, 0, 0, 1]])
+    R = eye(Y.shape[0])
+    
+    (X, P, K, IM, IS, LH) = kf_update(X, P, Y, H, R)
+    
+    (X, P) = kf_predict(X, P, A, Q, B, U)
+    
+    return (X, P)
